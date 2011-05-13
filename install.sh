5834766fcb8479dbf5ced14208e5cfc30c8ff9f5
René Haber <rene@habr.de>
2011-05-13 15:48:18 +0200

    Fixed install script.

--- install.sh
+++ install.sh
@@ -2,20 +2,13 @@
 
 # Very simple install script
 
-for file in `find ${PWD} -maxdepth 1 -not -name '.*' -not -name install.sh -not -name bin -not -name tasks -not -name etc -not README.txt -not LICENSE -type f`; do
+for file in `find ${PWD} -maxdepth 1 -not -name '.*' -not -name install.sh -not -name bin -not -name tasks -not -name etc`; do
+    if [ "${file}" == "${PWD}" ]
+    then
+        continue
+    fi
     filename=`basename "$file"`
     echo "ln -s `pwd`/$filename -> ~/.$filename"
     ln -s `pwd`/$filename ~/.$filename
 done
 
-mkdir -p ~/bin/
-for file in `find ${PWD}/bin/ -maxdepth 1 -not -name '.*'`; do
-    filename=`basename "$file"`
-    echo "ln -s `pwd`/$filename -> ~/bin/$filename"
-    ln -s `pwd`/$filename ~/bin/$filename
-done
-
-# Special case: Make bin/git-reup a link to git-up
-if [[-e ~/bin/git-up ]]; then
-    ln -s ~/bin/git-up ~/bin/git-reup
-fi
